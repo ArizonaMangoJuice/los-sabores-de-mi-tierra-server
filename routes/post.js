4b4c0f4a62404c68dc68a90b14d29446aa7f8ee0
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2020-12-23 13:31:56 -0700

    added public endpoint to get posts

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,5 @@
 'use strict';
+//will prob update to use async await
 
 const express = require('express');
 const passport = require('passport');
@@ -8,6 +9,14 @@ const Post = require('../models/post');
 
 const router = express.Router();
 // this prevents unauthorized user to access endpoint
+router.get('/', (req,res) =>{
+    Post
+        .find({})
+        .then(result => {
+            res.json(result);
+        })
+});
+
 router.use('/', passport.authenticate('jwt', {session: false, failWithError: true}));
 
 router.post('/', (req,res) => {
